da7209b223bb54ead8fc0fbaa4ad616978090537
Your Name <you@example.com>
2021-10-22 20:42:55 -0300

    asd

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const port = process.env.APP_PORT || 3000
+const fetch = require("node-fetch")
+var fetchJson = (url, options) => new Promise(async (resolve, reject) => {
+    fetch(url, options)
+        .then(response => response.json())
+        .then(json => {
+            // console.log(json)
+            resolve(json)
+        })
+        .catch((err) => {
+            reject(err)
+        })
+  })
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/:urlp/:urlp2', async (req, res) => {
+    const { urlp, urlp2 } = req.params
+    console.log(urlp)
+    fetchJson("http://" + urlp + "/" + urlp2)
+
+    res.send(await fetchJson("http://" + urlp + "/" + urlp2))
+  })
+
+  app.post('/:urlp/:urlp2', async (req, res) => {
+    const { urlp, urlp2 } = req.params
+    fetchJson("http://" + urlp  + "/" + urlp2, {method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: req.body})
+
+    res.send(await fetchJson("http://" + urlp  + "/" + urlp2, {method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: req.body}))
+  })
+
+  app.options('/:urlp', async (req, res) => {
+    const { urlp } = req.params
+    fetchJson(urlp)
+
+    res.send("OK")
+  })
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
